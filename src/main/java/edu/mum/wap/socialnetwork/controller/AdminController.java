b66309d6cd55c250ca835dd917f707bcfdc30c1f
turtogtokh <tmunkhtsooj@mum.edu>
2019-09-22 17:27:19 -0500

    admin jsp first draft

--- src/main/java/edu/mum/wap/socialnetwork/controller/AdminController.java
+++ src/main/java/edu/mum/wap/socialnetwork/controller/AdminController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class AdminController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        
 
     }
 
