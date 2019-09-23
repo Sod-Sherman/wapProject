4ee490f613f53df19681faf1aeb5f8ebfdaf755c
Sod Shirmen <sshirmen@mum.edu>
2019-09-23 15:36:38 -0500

    123

--- src/main/java/edu/mum/wap/socialnetwork/controller/LoginController.java
+++ src/main/java/edu/mum/wap/socialnetwork/controller/LoginController.java
@@ -5,6 +5,7 @@ import edu.mum.wap.socialnetwork.repository.PostRepositoryImpl;
 import edu.mum.wap.socialnetwork.repository.UserRepositoryImpl;
 import edu.mum.wap.socialnetwork.service.UserService;
 import edu.mum.wap.socialnetwork.service.impl.UserServiceImpl;
+
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,12 +18,14 @@ import javax.servlet.http.HttpSession;
 @WebServlet({"/login"})
 public class LoginController extends HttpServlet {
     private UserService userService = new UserServiceImpl();
+
     @Override
     public void init() throws ServletException {
         UserRepositoryImpl.getInstance();
         PostRepositoryImpl.getInstance();
 
     }
+
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doGet(req, resp);
     }
@@ -30,23 +33,42 @@ public class LoginController extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        String usernameInput = req.getParameter("username");
-        String passwordInput = req.getParameter("pass");
-        User newUser = new User();
-        newUser.setUsername(usernameInput);
-        newUser.setPassword(passwordInput);
-        String status = "unSuccess";
-        if (userService.login(newUser) != 0) {
-            status = "Success";
-            session.setAttribute("loggedInUser", newUser);
-        }else {
-            status = "password or username incorrect";
+        RequestDispatcher rd = null;
+
+        if (session.getAttribute("loggedInUser") == null) {
+
+            String usernameInput = req.getParameter("username");
+
+            if ("".equals(usernameInput) || usernameInput == null)
+                rd = req.getRequestDispatcher("login.jsp");
+            else {
+
+                String passwordInput = req.getParameter("pass");
+                User newUser = new User();
+                newUser.setUsername(usernameInput);
+                newUser.setPassword(passwordInput);
+                String status = "unSuccess";
+                Integer returnValue = userService.login(newUser);
+
+                if (returnValue != 0) {
+                    status = "Success";
+                    session.setAttribute("loggedInUser", userService.findByUsername(newUser.getUsername()));
+                    if (returnValue == 2)
+                        rd = req.getRequestDispatcher("admin.jsp");
+                    else
+                        rd = req.getRequestDispatcher("/timeline");
+                } else {
+                    status = "password or username incorrect";
+                    session.setAttribute("status", status);
+                    rd = req.getRequestDispatcher("login.jsp");
+                }
+            }
+        } else {
+            rd = req.getRequestDispatcher("/timeline");
         }
 
-        session.setAttribute("status", status);
-        resp.sendRedirect("login-success.jsp");
-//        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/timeline");
-//        requestDispatcher.forward(req,resp);
-//        resp.sendRedirect("timeline.jsp");
+
+        rd.forward(req, resp);
+
     }
 }
