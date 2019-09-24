f3602b93a262c7ad97905e86131a19fa1b7fe112
git2hub17 <swpuujee@gmail.com>
2019-09-24 15:22:22 -0500

    update - 24/09/19...Two Update profile changed-update

--- src/main/java/edu/mum/wap/socialnetwork/controller/ProfileController.java
+++ src/main/java/edu/mum/wap/socialnetwork/controller/ProfileController.java
@@ -8,6 +8,7 @@ package edu.mum.wap.socialnetwork.controller;
 import edu.mum.wap.socialnetwork.model.User;
 import edu.mum.wap.socialnetwork.service.UserService;
 import edu.mum.wap.socialnetwork.service.impl.UserServiceImpl;
+
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -28,71 +29,32 @@ public class ProfileController extends HttpServlet {
         HttpSession session = req.getSession();
         RequestDispatcher rd = null;
 
-        String firstNameInput = req.getParameter("name");
-        System.out.println(firstNameInput + "name");
-        String lastNameInput = req.getParameter("lastName");
-        String usernameInput = req.getParameter("username");
-        String phoneInput = req.getParameter("phone");
-        String emailInput = req.getParameter("email");
-        String locationInput = req.getParameter("location");
-        String passwordInput = req.getParameter("password");
-        System.out.println(session.getAttribute("loggedInUser"));
-        System.out.println("test");
-        if (session.getAttribute("loggedInUser") == null) {
-            if (!"".equals(firstNameInput) && firstNameInput != null) {
-                if (!"".equals(lastNameInput) && lastNameInput != null) {
-                    if (!"".equals(usernameInput) && usernameInput != null) {
-                        if (!"".equals(phoneInput) && phoneInput != null) {
-                            if (!"".equals(emailInput) && emailInput != null) {
-                                if (!"".equals(locationInput) && locationInput != null) {
-                                    if (!"".equals(passwordInput) && passwordInput != null) {
-                                        User profileUser = new User();
-                                        profileUser.setFirstName(firstNameInput);
-                                        profileUser.setLastName(lastNameInput);
-                                        profileUser.setUsername(usernameInput);
-                                        profileUser.setPhone(phoneInput);
-                                        profileUser.setEmail(emailInput);
-                                        profileUser.setLocation(locationInput);
-                                        profileUser.setPassword(passwordInput);
-                                        String statusProfile = "UnSuccess";
-                                        Integer returnValue = this.userService.updateProfile(profileUser);
-                                        if (returnValue != 0) {
-                                            statusProfile = "Success";
-                                            session.setAttribute("loggedInUser", this.userService.findByUsername(profileUser.getUsername()));
-                                            if (returnValue == 2) {
-                                                rd = req.getRequestDispatcher("profile.jsp");
-                                            } else {
-                                                statusProfile = "Account is not changed";
-                                                session.setAttribute("statusProfile", statusProfile);
-                                                rd = req.getRequestDispatcher("profile.jsp");
-                                            }
-                                        }
-                                    } else {
-                                        rd = req.getRequestDispatcher("profile.jsp");
-                                    }
-                                } else {
-                                    rd = req.getRequestDispatcher("profile.jsp");
-                                }
-                            } else {
-                                rd = req.getRequestDispatcher("profile.jsp");
-                            }
-                        } else {
-                            rd = req.getRequestDispatcher("profile.jsp");
-                        }
-                    } else {
-                        rd = req.getRequestDispatcher("profile.jsp");
-                    }
-                } else {
-                    rd = req.getRequestDispatcher("profile.jsp");
-                }
-            } else {
-                rd = req.getRequestDispatcher("profile.jsp");
-            }
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+
+
+        if (loggedInUser != null) {
+
+            req.setAttribute("loggedInUser", loggedInUser);
+
+            User tempUser = new User(
+                    req.getParameter("firstName"),
+                    req.getParameter("lastName"),
+                    req.getParameter("password"),
+                    req.getParameter("email"),
+                    req.getParameter("location"),
+                    req.getParameter("phone"));
+
+            tempUser.setUsername(loggedInUser.getUsername());
+
+            userService.updateProfile(tempUser);
+
+            rd = req.getRequestDispatcher("profile.jsp");
+
         } else {
-            rd = req.getRequestDispatcher("/timeline");
+            rd = req.getRequestDispatcher("login.jsp");
         }
 
-        System.out.println("test");
+        //System.out.println("test");
         rd.forward(req, resp);
     }
 
